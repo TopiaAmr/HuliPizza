c0ebd1e654e3f4cd70b633225e8384efb5d5720c
Amr Elsaadny <78759721+TopiaAmr@users.noreply.github.com>
2021-09-08 13:17:25 +0200

    Initial Commit

---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Topia Amr on 9/8/21.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
